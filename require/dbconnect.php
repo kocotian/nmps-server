6767a94e39c59e1fb4106c335268807b2d969bcf
kocotian <kocotian@kocotian.pl>
2020-11-09 21:30:34 +0100

    dbconnect tweak

--- require/dbconnect.php
+++ require/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-    return
-    [
-        "host"     => "<MYSQL DATABASE HOST>",
-        "username" => "<MYSQL DATABASE USERNAME>",
+	return
+	[
+		"host"     => "<MYSQL DATABASE HOST>",
+		"username" => "<MYSQL DATABASE USERNAME>",
 		"password" => "<MYSQL DATABASE PASSWORD>",
-        "database" => "<MYSQL DATABASE NAME>"
-    ];
+		"database" => "<MYSQL DATABASE NAME>"
+	];
