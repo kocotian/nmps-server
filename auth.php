a376024d0427385fee5d998836f4365114f32301
kocotian <kocotian@kocotian.pl>
2020-11-11 21:16:16 +0100

    authorization changes + new commands

--- auth.php
+++ auth.php
@@ -7,7 +7,7 @@
 	$user = db::query("SELECT * FROM users WHERE username=:username", ['username' => $argv[0]])[0];
 	if ($user)
 		if (password_verify($argv[1], $user['password']))
-			echo "Authorized";
+			echo "Authorized\naa13d2b75605c3472101c195c5cd1cb601daa320";
 		else
 			echo "Wrong password";
 	else
